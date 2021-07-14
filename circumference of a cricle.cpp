9daa98f346a3719ca38f6509d082eff271012057
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-15 02:27:10 +0500

    circumference of a circle

---
+++ circumference of a cricle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r;
+	double cir;
+	float pi=3.14;
+	cin>>r;
+	cout<<"enter the value of a radius:"<<r<<endl;
+	cir=2*pi*r;
+	cout<<"the circumference of a circle is"<<cir<<endl;
+	return(0);
+}
