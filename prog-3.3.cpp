ae387a4e36aff64ae691fde24001755436f4ccd9
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-16 00:39:54 +0500

    Add files via upload

---
+++ prog-3.3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=78;// will print a value of integers:
+	char ch='a';// declare a character in a string function
+	// delcare a one character:
+	cout<<"hello my name is abdul ahad raza from rawalpindi thank you:"<<endl;//output statement:
+	cout<<ch<<endl;
+	cout<<a<<endl;
+	return(0);
+}
