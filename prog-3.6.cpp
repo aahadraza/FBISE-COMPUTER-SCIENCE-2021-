f2294dbe296be8806ef0ffe40b21df58612cca03
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-16 00:41:02 +0500

    Add files via upload

---
+++ prog-3.6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	cout<<"\a my name is abdul ahad raza"<<endl;
+	cout<<"\b my name is ahsan raza:"<<endl;
+	cout<<"\n my name is shoukat raza:"<<endl;
+	cout<<"\r my name is adnan raza:"<<endl;
+	cout<<"\t my name is ahyan raza:"<<endl;
+	cout<<"\\ my name is shazil sultan"<<endl;
+	cout<<"\' my name is ali raza"<<endl;
+	cout<<"\" my name is bilal munir\""<<endl;
+	return(0);
+}
