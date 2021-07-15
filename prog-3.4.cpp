3ea1830139f5b440ea3eca11ab7dfe4021517c24
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-16 00:40:14 +0500

    Add files via upload

---
+++ prog-3.4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a;
+	float b;
+	float sum;
+	cout<<"enter the value of a:";
+	cin>>a;
+	cout<<"enter the value of b:";
+	cin>>b;
+	sum=a+b;
+	cout<<"the sum of two floating point is"<<sum<<endl;
+	return(0);
+}
