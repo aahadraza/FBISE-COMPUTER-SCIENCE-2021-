1c76b1a86659955967fed4089a49d3bc63b30c0a
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-16 13:42:46 +0500

    Add files via upload

---
+++ program 13.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<setw(10)<<"number";
+	cout<<setw(10)<<"square";
+	cout<<setw(10)<<"cube";
+	cout<<setw(10)<<1<<setw(10)<<1*1<<setw(10)<<1*1*1<<endl;
+	cout<<setw(10)<<2<<setw(10)<<2*2<<setw(10)<<2*2*2<<endl;
+	cout<<setw(10)<<3<<setw(10)<<3*3<<setw(10)<<3*3*3<<endl;
+	cout<<setw(10)<<4<<setw(10)<<4*4<<setw(10)<<4*4*4<<endl;
+	cout<<setw(10)<<5<<setw(10)<<5*5<<setw(10)<<5*5*5<<endl;
+	cout<<setw(10)<<6<<setw(10)<<6*6<<setw(10)<<6*6*6<<endl;
+	return(0);
+}
