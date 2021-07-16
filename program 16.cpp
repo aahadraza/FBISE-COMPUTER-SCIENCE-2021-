273457b313bbc1412084df79e9068aa8c2e33d51
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-16 13:45:10 +0500

    Add files via upload

---
+++ program 16.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	cout<<"enter the value of a:"<<endl;
+	cin>>a;
+	cout<<"enter the value of b:"<<endl;
+	cin>>b;
+	cout<<"a+b:"<<a+b<<endl;
+	cout<<"a*b:"<<a*b<<endl;
+	cout<<"a-b:"<<a-b<<endl;
+	cout<<"a/b:"<<a/b<<endl;
+	cout<<"a%b:"<<a%b<<endl;
+	cout<<"a+b/2:"<<a+b/2<<endl;
+	return(0);
+}
