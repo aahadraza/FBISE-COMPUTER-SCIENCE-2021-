f7d391a159ec4293ef873002f6ae118eeab9ec99
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-16 13:46:35 +0500

    Add files via upload

---
+++ PROGRAM21.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter your number of a:";
+	cin>>a;
+	cout<<"+="<<a+=10<<endl;
+	cout<<"-="<<a-=10<<endl;
+	cout<<"*="<<a*=10<<endl;
+	cout<<"/="<<a/=10<<endl;
+	cout<<"%="<<a%=10<<endl;
+	return(0);
+}
