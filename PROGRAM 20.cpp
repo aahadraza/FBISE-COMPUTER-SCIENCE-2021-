6cd5f2f79a8fbdc0e5252f65432d75e9f22165be
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-16 13:46:20 +0500

    Add files via upload

---
+++ PROGRAM 20.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int base;
+	int height;
+	int area;
+	cout<<"enter the value of base:"<<endl;
+	cin>>base;
+	cout<<"enter the value of height:"<<endl;
+	cin>>height;
+	area=base*height;
+	cout<<"now the area of the rectangle:"<<area<<endl;
+	return(0);
+}
