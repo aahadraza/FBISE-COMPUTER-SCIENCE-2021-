22cf05b3c5527508f81077391b4c9a0aa9f6ceea
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-17 19:52:23 +0500

    Add files via upload

---
+++ what will the output.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1,num2,total;
+	cout<<"enter the value of num1:"<<endl;
+	cin>>num1;
+	cout<<"enter the value of num2:"<<endl;
+	cin>>num2;
+	total=num1+num2;
+	cout<<"the answer of total is:"<<total<<"the value of variable 1 is:"<<num1<<endl;
+	return(0);
+}
