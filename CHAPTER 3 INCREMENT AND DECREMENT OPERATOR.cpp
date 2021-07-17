18b5201256207e6f90ec4d30e24a98145d7fe754
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-07-17 19:39:54 +0500

    Add files via upload

---
+++ CHAPTER 3 INCREMENT AND DECREMENT OPERATOR.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    a=b=c=d=2;
+    a++;
+    b++;
+    c++;
+    d++;
+    cout<<"a:"<<a<<endl;
+    cout<<"b:"<<b<<endl;
+    cout<<"c:"<<c<<endl;
+    cout<<"d:"<<d<<endl;
+    return(0);
+}
